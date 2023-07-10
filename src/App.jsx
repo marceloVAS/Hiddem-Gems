f6e0df596cd2acb91b0323895b95d5d8c292dfd7
Marcelo Sousa <marcelo.victor010@gmail.com>
2023-07-09 23:17:40 -0400

    main structure changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import AuthFinal from './components/AuthFinal';
+import Header from './components/Header';
 import './App.css';
 
 export default function App() {
   return (
-    <div>
+    <>
+    <Header />
+    <div className='container'>
       <AuthFinal/>
     </div>
+    </>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './UI/Header.css'
+
+export default function Header(){
+
+    return(
+        <div className='row header mb-5'>
+            <div className='logo'>
+                <h1>Hidden Gems</h1>
+            </div>
+             <nav class="navbar navbar-expand-lg ">
+                <div class="container-fluid">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
+                        <ul class="navbar-nav ">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}

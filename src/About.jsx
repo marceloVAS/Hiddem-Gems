c8345906a751cbc691b16888849d632de8f245cf
Marcelo Sousa <marcelo.victor010@gmail.com>
2023-07-11 21:35:29 -0400

    added about page

---
+++ src/About.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+import React from 'react';
+import Header from './components/Header';
+
+export default function About() {
+
+    return (
+        <>
+    <Header />
+    <div className='container'>
+      {/* <AuthFinal/> */}
+    </div>
+    </>
+    )
+}

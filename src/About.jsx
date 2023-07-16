fdc98ae0a14441f4aaf4f57bbe8711a335f58775
Marcelo Sousa <marcelo.victor010@gmail.com>
2023-07-16 00:17:52 -0400

    footer added about content updated, App container padding added

--- src/About.jsx
+++ src/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
     return (
       <>
         <Header />
-        <div className='container d-flex justify-content-center'>
+        <div className='container mb-5 info-height d-flex justify-content-center'>
           <div className='card info'>
             <p>
               Hidden Gems is a work in progress built using React.js, Vite and Spotify API by Marcelo Sousa. It is a personal project to help people find musical hidden gems. 
@@ -15,13 +15,13 @@ export default function About() {
             </p>
             <br/>
             <p>
-              I had this idea bouncing around my head for a while already and since I would love a tool like this I decided to finally build it. Since it is a work in progress and a personal project consider leaving some feedback if you find any issues or have some ideas, I'd love to hear them.
+              I had this idea bouncing around my head for a while already and since I would love a tool like this I decided to finally build it. Since this is a work in progress and a personal project consider leaving some feedback if you find any issues or have some ideas, I'd love to hear them.
             </p>
             <br />
             <p>Hope you enjoy it and good digging :)</p>
           </div>
         </div>
-        {/* <Footer /> */}
+        <Footer />
       </>
     )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import AuthFinal from './components/AuthFinal';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import './App.css';
 
 export default function App() {
   return (
     <>
     <Header />
-    <div className='container'>
+    <div className='container pb-5'>
       <AuthFinal/>
     </div>
+    <Footer />
     </>
   );
 }

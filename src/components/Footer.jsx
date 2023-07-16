8d919dca3103bbee3837824df81efece8ad20b5d
Marcelo Sousa <marcelo.victor010@gmail.com>
2023-07-16 00:21:52 -0400

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,22 +3,20 @@ import './UI/Footer.css'
 
 export default function Header() {
     return (
-        <div className='row header mb-5'>
-            <nav className="navbar navbar-expand-lg ">
-                <div className="container d-flex justify-content-sm-end justify-content-center">
-                    <div>
-                        <ul className="navbar-nav ">
-                            <li className="nav-item">
-                                <a className="nav-link active" href="#">HOME</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">ABOUT</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" aria-current="page" href="#">FEEDBACK</a>
-                            </li>
-                        </ul>
-                    </div>
+        <div className='row'>
+            <nav className="navbar footer">
+                <div className="container d-flex justify-content-start">
+                <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <p className='footer-txt'>v1 | built by Marcelo Sousa</p>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href='#'>LINKEDIN</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" href='#'>PORTFOLIO</a>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </div>

ff26186c8f60423414ffc42b8ce5035f2650cb3d
Marcelo Sousa <marcelo.victor010@gmail.com>
2023-08-29 20:01:22 -0400

    hotfix to the redirect address after login

--- src/components/AuthFinal.jsx
+++ src/components/AuthFinal.jsx
@@ -18,8 +18,8 @@ const codeConditional = async () => {
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
-    params.append("redirect_uri", "http://localhost:5173");
-    //params.append("redirect_uri", "https://hiddengems.marcelovas.com");
+    //params.append("redirect_uri", "http://localhost:5173");
+    params.append("redirect_uri", "https://hiddengems.marcelovas.com");
     params.append("scope", "");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
@@ -64,8 +64,8 @@ const getAccessToken = async (clientId, code) => {
   params.append("client_id", clientId);
   params.append("grant_type", "authorization_code");
   params.append("code", code);
-  params.append("redirect_uri", "http://localhost:5173");
-  //params.append("redirect_uri", "https://hiddengems.marcelovas.com");
+  //params.append("redirect_uri", "http://localhost:5173");
+  params.append("redirect_uri", "https://hiddengems.marcelovas.com");
   params.append("code_verifier", verifier);
 
   const result = await fetch("https://accounts.spotify.com/api/token", {

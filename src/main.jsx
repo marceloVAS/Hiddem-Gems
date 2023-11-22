a8c813d8f1623807c823c19a3e4bac086c8c1566
Marcelo Sousa <marcelo.victor010@gmail.com>
2023-11-22 17:51:50 -0500

    added GA4 tracking tag and necessary packages

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import { createBrowserRouter, RouterProvider, } from "react-router-dom";
 import App from './App.jsx'
 import About from './About.jsx'
 import './index.css'
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-96FFN4P2GY");
 
 const router = createBrowserRouter([
     {
